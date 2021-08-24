e29250f2486df4715725ebbe198d3b849e018b77
Tim Carver <tfc13b@gmail.com>
2021-08-24 14:08:18 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
