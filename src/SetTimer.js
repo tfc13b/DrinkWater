bbac181d41e5323d4ba285b40e6cbac646f772cc
Tim Carver <tfc13b@gmail.com>
2021-10-15 15:56:44 -0400

    Update SetTimer.js

--- src/SetTimer.js
+++ src/SetTimer.js
@@ -1,53 +1,68 @@
 import * as React from "react";
 import { Input } from "baseui/input";
-import { Button } from "baseui/button";
+import './App.css';
 
-/*export default function MyCustomeField({
-  initialValue= 30,
-  placeholder= 'Enter Timer in Minutes...'
-}){
-  const [value, setValue] = React.useState(initialValue);
-  return (
-    <div><Input
-      placeholder={placeholder}
-      value={value}
-      onChange={e => setValue(e.target.value)}
-      clearOnEscape />
-      <Button onClick={() => setValue(value)}>Set Timer</Button></div>
-  )
-};
-*/
-export default class SetTimer extends React.Component {
+export default class setTimer extends React.Component {
+
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      value: 'Set Time in Minutes..',
-    };
+    this.state = { timer: 30 };
   }
 
-  handleChange = (event) => {
-    this.setState({
-      value: event.target.value
-    });
+  handleChange = event => {
+    this.setState({ timer: event.target.value });
   };
 
- handleClick = () => {
-    this.setState({
-      value:30
-    });
-  };
-
-  render() {
+    render() {
+      const setClock = setInterval(notifyMe, this.state.timer*60000);
     return (
-      <div>
-        <Input
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-        <Button onClick={this.handleClick}>Set Time in Minutes</Button>
-      </div>
+         <div className="App">
+        <header className="App-header">
+          <img src="https://rm7ix2r8hzo4121x321k81m1-wpengine.netdna-ssl.com/wp-content/uploads/2020/08/Children-and-Water-Content-1024x536.jpg" className="App-logo" alt="logo" />
+          <h1 onLoad={setClock}>
+            Drink Water!!
+          </h1>
+        </header>
+        <div>
+          <Input
+            value={this.state.timer}
+            onChange={this.handleChange}
+          />
+          </div>
+        </div>
     );
+
+  }
+}
+
+console.log(Notification.permission)
+function notifyMe() {
+  if (!window.Notification) {
+      console.log('Browser does not support notifications.');
+  } else {
+      // check if permission is already granted
+      if (Notification.permission === 'granted') {
+          // show notification here
+          var notify = new Notification('Hi there!', {
+              body: 'Drink More Water :)',
+              icon: 'https://rm7ix2r8hzo4121x321k81m1-wpengine.netdna-ssl.com/wp-content/uploads/2020/08/Children-and-Water-Content-1024x536.jpg',
+          });
+      } else {
+          // request permission from user
+          Notification.requestPermission().then(function (p) {
+              if (p === 'granted') {
+                  // show notification here
+                  var notify = new Notification('Hi there!', {
+                      body: 'Dink More Water :)',
+                      icon: 'https://rm7ix2r8hzo4121x321k81m1-wpengine.netdna-ssl.com/wp-content/uploads/2020/08/Children-and-Water-Content-1024x536.jpg',
+                  });
+              } else {
+                  console.log('User blocked notifications.');
+              }
+          }).catch(function (err) {
+              console.error(err);
+          });
+      }
   }
 }
