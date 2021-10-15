defe2d56e7a56e37d450e88e12db6b5f049969f9
Tim Carver <tfc13b@gmail.com>
2021-10-15 15:57:37 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SetTimer from './SetTimer.js';
 
 ReactDOM.render(
-    <App />,
+  <SetTimer/>,
   document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
